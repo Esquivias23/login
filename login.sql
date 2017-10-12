b83a4ca5ae579d31dddf374c0c18fc44dc79fb95
Esquivias23 <30818529+Esquivias23@users.noreply.github.com>
2017-10-12 07:21:54 -0500

    Update login.sql

--- login.sql
+++ login.sql
@@ -16,9 +16,13 @@ delimiter **
 create procedure guardaUsuario(in neim nvarchar(15),in pass nvarchar(15))
 begin
 declare msj nvarchar(20);
-	insert into tablita(nombre, pass)
-    values (neim, pass);
-	set msj = 'kmara';
+declare idU int; 
+	set idU = (select ifnull(max(idUsuario), 0) + 1 from Usuario);
+	insert into Usuario(idUsuario, nombre, pass)
+    values (idU, neim, pass);
+	set msj = 'Usuario Listo';
+    
+    select msj as 'Resultado';
 
 end; **
 delimiter ;
